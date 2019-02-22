cc0824bee3721c82c445475e50f88ba92b82db81
NorwellFram <46940569+NorwellFram@users.noreply.github.com>
2019-02-22 16:13:32 -0500

    closes #2 ajout de temperature en base de donnée

--- app/src/main/java/org/appducegep/mameteo/MeteoDAO.java
+++ app/src/main/java/org/appducegep/mameteo/MeteoDAO.java
@@ -11,7 +11,7 @@ import java.util.Date;
 // https://developer.android.com/training/data-storage/sqlite.html
 
 public class MeteoDAO extends SQLiteOpenHelper {
-    public static final int DATABASE_VERSION = 2;
+    public static final int DATABASE_VERSION = 3;
     public static final String DATABASE_NAME = "Meteo.db";
 
     public MeteoDAO(Context context) {
@@ -21,17 +21,26 @@ public class MeteoDAO extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        String SQL_CREATION_TABLE = "create table meteo(id INTEGER PRIMARY KEY, ville TEXT, soleilOuNuage TEXT, date TEXT)";
-        db.execSQL(SQL_CREATION_TABLE);
+        String SQL_CREATION_TABLE_1 = "create table meteo(id INTEGER PRIMARY KEY, ville TEXT, soleilOuNuage TEXT, date TEXT)";
+        String SQL_CREATION_TABLE_2 = "create table meteo(id INTEGER PRIMARY KEY, ville TEXT, soleilOuNuage TEXT, date TEXT, vent TEXT, umidite Integer)";
+        String SQL_CREATION_TABLE_3 = "create table meteo(id INTEGER PRIMARY KEY, ville TEXT, soleilOuNuage TEXT, date TEXT, vent TEXT, umidite Integer, temperature REAL)";
+        db.execSQL(SQL_CREATION_TABLE_3);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        String SQL_MISEAJOUR_TABLE = "alter table meteo add column vent TEXT";
-        db.execSQL(SQL_MISEAJOUR_TABLE);
-        String SQL_MISEAJOUR_TABLE_B = "alter table meteo add column humidite Integer";
-        db.execSQL(SQL_MISEAJOUR_TABLE_B);
+        String SQL_MISEAJOUR_TABLE_1V2 = "alter table meteo add column vent TEXT";
+        String SQL_MISEAJOUR_TABLE_1V2_B = "alter table meteo add column humidite Integer";
+        String SQL_MISEAJOUR_TABLE_2V3 = "alter table meteo add column temperature REAL";
+        if(oldVersion==1){
+            db.execSQL(SQL_MISEAJOUR_TABLE_1V2);
+            db.execSQL(SQL_MISEAJOUR_TABLE_1V2_B);
+        }
+        if(newVersion==3){
+            db.execSQL(SQL_MISEAJOUR_TABLE_2V3);
+        }
+
     }
 
     public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
@@ -40,7 +49,7 @@ public class MeteoDAO extends SQLiteOpenHelper {
 
     }
 
-    public void ajouterMeteo(String soleilOuNuage, int humidite, String vent)
+    public void ajouterMeteo(String soleilOuNuage, int humidite, String vent, float temperature)
     {
         //Date aujourdhui = new Date();
         SQLiteDatabase db = this.getWritableDatabase();
@@ -48,8 +57,9 @@ public class MeteoDAO extends SQLiteOpenHelper {
         meteoDuJour.put("ville", "Matane");
         meteoDuJour.put("soleilOuNuage", soleilOuNuage);
         meteoDuJour.put("date", DateFormat.format("MMMM d, yyyy ", (new Date()).getTime()).toString());
-        meteoDuJour.put("vent: ", vent);
-        meteoDuJour.put("humidite: ", humidite);
+        meteoDuJour.put("vent", vent);
+        meteoDuJour.put("humidite", humidite);
+        meteoDuJour.put("temperature", temperature);
         long newRowId = db.insert("meteo", null, meteoDuJour);
 
     }
